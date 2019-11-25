ba1876d980031fa95444681664b1ff1904b8553b
feebeef <phoebe_chen@dlsu.edu.ph>
2019-11-26 03:12:01 +0800

    init files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const session = require("express-session")
+const bodyparser = require("body-parser")
+const hbs = require("hbs")
+const url = require("url")
+const cryptojs = require("crypto-js")
+
+const app = new express();
+var path = require("path");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(session({
+    resave : true,
+    name :"treehouseyoga",
+    saveUninitialized : true,
+    secret : "secretpass",
+    cookie : {
+        maxAge: 10*60*60*1000
+    }
+}));
+
+app.use(urlencoder)
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'views'))
+app.use(require("./controller"))
+app.listen(3000,()=>{
+    console.log("Live at port 3000");
+})
